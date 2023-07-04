0595001f41c1a2b3e1e893faac271f8d7d33a70f
bigzhu <vermiliondun@gmail.com>
2023-07-05 01:28:07 +0800

    make flutter-tools work

--- plugins.lua
+++ plugins.lua
@@ -32,41 +32,7 @@ local plugins = {
     "akinsho/flutter-tools.nvim",
     after = "nvim-lspconfig",
     config = function()
-      local on_attach = require("plugins.configs.lspconfig").on_attach
-      local capabilities = require("plugins.configs.lspconfig").capabilities
-
-      local present, flutter_tools = pcall(require, "flutter-tools")
-      if not present then
-        return
-      end
-
-      flutter_tools.setup {
-        dev_log = {
-          enabled = false,
-          open_cmd = "tabedit", -- command to use to open the log buffer
-        },
-
-        dev_tools = {
-          autostart = true, -- autostart devtools server if not detected
-          auto_open_browser = false, -- Automatically opens devtools in the browser
-        },
-
-        lsp = {
-          color = {
-            enabled = true,
-          },
-
-          on_attach = on_attach,
-          capabilities = capabilities,
-
-          settings = {
-            showTodos = true,
-            completeFunctionCalls = true,
-            renameFilesWithClasses = "prompt",
-            enableSnippets = true,
-          },
-        },
-      }
+      require "custom.plugins.fluttertools"
     end,
   },
   {
