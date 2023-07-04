09d06a1660fdf79ef6f3fbef438494e700bfffdf
bigzhu <vermiliondun@gmail.com>
2023-07-04 21:59:56 +0800

    init

---
+++ plugins.lua
@@ -0,0 +1,106 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "akinsho/flutter-tools.nvim",
+    after = "nvim-lspconfig",
+    config = function()
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local present, flutter_tools = pcall(require, 'flutter-tools')
+if not present then
+  return
+end
+
+flutter_tools.setup {
+  dev_log = {
+    enabled = false,
+    open_cmd = "tabedit", -- command to use to open the log buffer
+  },
+
+  dev_tools = {
+    autostart = true, -- autostart devtools server if not detected
+    auto_open_browser = false, -- Automatically opens devtools in the browser
+  },
+
+  lsp = {
+    color = {
+      enabled = true,
+    },
+
+    on_attach = on_attach,
+    capabilities = capabilities,
+
+    settings = {
+      showTodos = true,
+      completeFunctionCalls = true,
+      renameFilesWithClasses = "prompt",
+      enableSnippets = true
+    }
+  }
+}
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
+
+  -- All NvChad plugins are lazy-loaded by default
+  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  -- }
+}
+
+return plugins
